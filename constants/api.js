85feadf047edca54d3e9f2ab286439491f629d6b
Yago <yagojf27@gmail.com>
2022-08-24 16:00:28 -0300

    first commit

--- constants/api.js
+++
@@ -1,4 +0,0 @@
-export const API_ROUTES = {
-    baseUrl: (api_key) => `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`,
-    imageUrl: (imageName) => `https://image.tmdb.org/t/p/w400/${imageName}`,
-};
--- constants/api.ts
+++
@@ -1,5 +0,0 @@
-export const API_ROUTES = {
-  baseUrl: (api_key: string) =>
-    `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`,
-  imageUrl: (imageName: string) => `https://image.tmdb.org/t/p/w400/${imageName}`,
-};
